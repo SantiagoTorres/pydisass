83a789cc66c4023a05e5a74deaffa04cf9f26589
Santiago Torres <torresariass@gmail.com>
2015-02-24 21:27:50 -0500

    Adds docstrings to the readelf classes

--- readelf.py
+++ readelf.py
@@ -3,6 +3,31 @@ import struct
 from elfconstants import E_TYPES, E_MACHINES, SECTION_HEADER_TYPES, \
     SHT_LOOS, SHT_HIOS, SHT_LOPROC, SHT_HIPROC, SHT_LOUSER, SHT_HIUSER
 
+
+"""
+    Class section_header
+
+    Includes information regarding a section header for an elf binary.
+
+    <Attributes>
+        _name: a string repesenting the name
+        _nameind: the index of the string representing its name in the string
+                  section
+        _type: the constant representing its type
+        _flags: flags for this section (read, write, etc.)
+        _addr: The location of this header
+        _offset: The offset from this header to its contents
+        _size: the size of the section
+        _link: I have no idea what this does
+        _align: The address it should be loaded into
+        _entsize: The size of this entry
+
+    <Methods>
+       init:        initializes from the values provided
+       fetch_name:  given a string table section, find our name and set the
+                    property
+       toString:    pretty print the available information
+"""
 class section_header:
     
     _name    = None  # string name
@@ -16,6 +41,13 @@ class section_header:
     _align   = None  # Address to be loaded into
     _entsize = None  # size of this entry
 
+
+    """
+        init method
+
+        this is a simple kwargs-copying constructor, nothing fancy.
+
+    """
     def __init__(self, kwargs):
     
         
@@ -51,6 +83,23 @@ class section_header:
 
         return None
 
+    """
+        fetch_name method:
+
+        When provided the contents to a string table, seek four the name of
+        this section and set it in this instance.
+
+        <parameters>
+            strtable: a byte-string containing the contents of the section to 
+                      seek
+
+        <returns>
+            the name of this section
+
+        <Side Effects>
+            The name of this instance's section is set
+
+    """
     def fetch_name(self, strtable):
     
         if self._type == 0:
@@ -64,6 +113,18 @@ class section_header:
 
         return self._name
 
+    """
+        toString method:
+
+        Pretty prints (read formats) the contents of this object for
+        printing.
+
+        <Parameters>
+            None
+
+        <Returns>
+            A string representation for this object for printing.
+    """
     def toString(self):
 
         returnstr = ""
@@ -87,6 +148,28 @@ class section_header:
 
         return returnstr
 
+
+"""
+    Class elf_executable
+
+    Holds the information of an executable, contains methods for parsing and
+    reading section headers and binary blobs of data.
+
+    <Attributes>
+        Header:         Contains information about the header, wordsize, 
+                        target platform, all that stuff
+
+        section_headers: Contains a list of section header objects
+
+        executable:      The binary blob of data for the loaded executable
+
+    <Methods>
+        __init__:        Given a stream of data, try to load it as an ELF.
+        read_elf_header: Read the header from an executable
+
+    <Notes>
+        TODO: The methods below should be part of the object in later stages
+    """
 class elf_executable:
 
     header = None
@@ -95,111 +178,111 @@ class elf_executable:
 
     def __init__(self, executable):
 
-        self.header = read_elf_header(executable)
+        self.header = self.read_elf_header(executable)
         self.section_headers = parse_elf_sections(executable, self.header)
         self.executable = executable
 
-def read_elf_header(executable):
-   
-    HEADER_STRINGS = [
-       "EI_MAGIC",        # Magic string, identifying the ELF header
-       "EI_CLASS",        # Wordsize
-       "EI_DATA",         # Endianness
-       "EI_VERSION",      # File version
-       "EI_OSABI",        # Operating system/ABI identification
-       "EI_ABIVERSION",   # ABI version
-       "E_TYPE",          # Type of executable
-       "E_MACHINE",       # Type of machine
-       "E_VERSION",       # Version, should be 1
-       "E_ENTRY",         # The program's entry point
-       "E_PHOFF",         # Offset until the start of the header table
-       "E_SHOFF",         # Offset to the start of the section header table
-       "E_FLAGS",         # This is architecture specific
-       "E_EHSIZE",        # Header size (should be 52 or 64)
-       "E_PHENTSIZE",     # Size of the program header table entry
-       "E_PHNUM",         # Number of entries in such table
-       "E_SHENTSIZE",     # Size of the section header table entry
-       "E_SHNUM",         # Number of entries in the section header 
-       "E_SHSTRNDX",      # Index of the section header entries (section names)
-    ]
+    def read_elf_header(self, executable):
+       
+        HEADER_STRINGS = [
+           "EI_MAGIC",        # Magic string, identifying the ELF header
+           "EI_CLASS",        # Wordsize
+           "EI_DATA",         # Endianness
+           "EI_VERSION",      # File version
+           "EI_OSABI",        # Operating system/ABI identification
+           "EI_ABIVERSION",   # ABI version
+           "E_TYPE",          # Type of executable
+           "E_MACHINE",       # Type of machine
+           "E_VERSION",       # Version, should be 1
+           "E_ENTRY",         # The program's entry point
+           "E_PHOFF",         # Offset until the start of the header table
+           "E_SHOFF",         # Offset to the start of the section header table
+           "E_FLAGS",         # This is architecture specific
+           "E_EHSIZE",        # Header size (should be 52 or 64)
+           "E_PHENTSIZE",     # Size of the program header table entry
+           "E_PHNUM",         # Number of entries in such table
+           "E_SHENTSIZE",     # Size of the section header table entry
+           "E_SHNUM",         # Number of entries in the section header 
+           "E_SHSTRNDX",      # Index of the section header entries (section names)
+        ]
 
-    parsed_header = {}
-    unpackerstr = "<bbbbbbbbb"
-    
-    header_data = struct.unpack(unpackerstr, bytes(executable[0:9]))
-    parsed_header[HEADER_STRINGS.pop(0)] = header_data[0:4]
-
-    for i in range(4, 9):
-        parsed_header[HEADER_STRINGS.pop(0)] = header_data[i]
-
-    strmagic = "Magic (ELF Header):"
-    for byte in parsed_header["EI_MAGIC"]:
-        strmagic += " {}".format(hex(byte))
-    print(strmagic)
-
-    if parsed_header["EI_CLASS"] == 0x01:
-        print("CLASS             : ELF32")
-        unpackerstr= "HHIIIIIHHHHHH"
-        headersize = 52
-
-    elif parsed_header["EI_CLASS"] == 0x02:
-        print("CLASS             : ELF64")
-        EI_CLASS = 0x2
-        unpackerstr = "HHIQQQIHHHHHH"
-        headersize = 64
+        parsed_header = {}
+        unpackerstr = "<bbbbbbbbb"
         
-    else:
-        raise Exception("Wrong EI_CLASS value! ({})".format(
-            hex(parsed_header["EI_CLASS"])))
+        header_data = struct.unpack(unpackerstr, bytes(executable[0:9]))
+        parsed_header[HEADER_STRINGS.pop(0)] = header_data[0:4]
 
-    if parsed_header["EI_DATA"] == 0x01:
-        print("EI_DATA           : ELFDATA2LSB")
-        byteorder = '<'
-    elif parsed_header["EI_DATA"] == 0x02:
-        print("EI_DATA           : ELFDATA2MSB")
-        byteorder = '>'
-    else:
-        raise Exception("Endianness is not defined!")
+        for i in range(4, 9):
+            parsed_header[HEADER_STRINGS.pop(0)] = header_data[i]
 
-    header_data = struct.unpack(byteorder + unpackerstr,
-            bytes(executable[16 : headersize]))
+        strmagic = "Magic (ELF Header):"
+        for byte in parsed_header["EI_MAGIC"]:
+            strmagic += " {}".format(hex(byte))
+        print(strmagic)
 
-    for field in header_data:
-        parsed_header[HEADER_STRINGS.pop(0)] = field
-    
-    print("EI_VERSION        : {}".format(hex(parsed_header["EI_VERSION"])))
-    print("EI_OSABI          : {}".format(hex(parsed_header["EI_OSABI"])))
-    print("EI_ABIVERSION     : {}".format(hex(parsed_header["EI_ABIVERSION"])))
+        if parsed_header["EI_CLASS"] == 0x01:
+            print("CLASS             : ELF32")
+            unpackerstr= "HHIIIIIHHHHHH"
+            headersize = 52
 
-    if parsed_header["E_TYPE"] not in E_TYPES:
-        raise Exception("Not a valid Executable format!")
+        elif parsed_header["EI_CLASS"] == 0x02:
+            print("CLASS             : ELF64")
+            EI_CLASS = 0x2
+            unpackerstr = "HHIQQQIHHHHHH"
+            headersize = 64
+            
+        else:
+            raise Exception("Wrong EI_CLASS value! ({})".format(
+                hex(parsed_header["EI_CLASS"])))
 
-    print("E_TYPE            : {}".format(E_TYPES[parsed_header["E_TYPE"]]))
+        if parsed_header["EI_DATA"] == 0x01:
+            print("EI_DATA           : ELFDATA2LSB")
+            byteorder = '<'
+        elif parsed_header["EI_DATA"] == 0x02:
+            print("EI_DATA           : ELFDATA2MSB")
+            byteorder = '>'
+        else:
+            raise Exception("Endianness is not defined!")
 
-    if parsed_header["E_MACHINE"] not in E_MACHINES:
-        raise Exception("Not a valid machine number! ({})".format(
-            E_MACHINE))
+        header_data = struct.unpack(byteorder + unpackerstr,
+                bytes(executable[16 : headersize]))
 
-    print("E_MACHINE         : {}".format(E_MACHINES[parsed_header["E_MACHINE"]]))
+        for field in header_data:
+            parsed_header[HEADER_STRINGS.pop(0)] = field
+        
+        print("EI_VERSION        : {}".format(hex(parsed_header["EI_VERSION"])))
+        print("EI_OSABI          : {}".format(hex(parsed_header["EI_OSABI"])))
+        print("EI_ABIVERSION     : {}".format(hex(parsed_header["EI_ABIVERSION"])))
 
-    if parsed_header["E_VERSION"] == 0x1:
-        print("E_VERSION         : 0x1 (VALID)")
-    else:
-        raise Exception("E_VERSION INVALID ({})".format(
-            parsed_header["E_VERSION"]))
+        if parsed_header["E_TYPE"] not in E_TYPES:
+            raise Exception("Not a valid Executable format!")
 
-    print("Entry Point       : {}".format(hex(parsed_header["E_ENTRY"])))
-    print("Header table off. : {}".format(hex(parsed_header["E_PHOFF"])))
-    print("Section header off: {}".format(hex(parsed_header["E_SHOFF"])))
-    print("E_FLAGS           : {}".format(hex(parsed_header["E_FLAGS"])))
-    print("E_EHSIZE          : {}".format(hex(parsed_header["E_EHSIZE"])))
-    print("E_PHENTSIZE       : {}".format(hex(parsed_header["E_PHENTSIZE"])))
-    print("E_PHNUM           : {}".format(hex(parsed_header["E_PHNUM"])))
-    print("E_SHENTSIZE       : {}".format(hex(parsed_header["E_SHENTSIZE"])))
-    print("E_SHNUM           : {}".format(hex(parsed_header["E_SHNUM"])))
-    print("E_SHSTRNDX        : {}".format(hex(parsed_header["E_SHSTRNDX"])))
+        print("E_TYPE            : {}".format(E_TYPES[parsed_header["E_TYPE"]]))
 
-    return parsed_header
+        if parsed_header["E_MACHINE"] not in E_MACHINES:
+            raise Exception("Not a valid machine number! ({})".format(
+                E_MACHINE))
+
+        print("E_MACHINE         : {}".format(E_MACHINES[parsed_header["E_MACHINE"]]))
+
+        if parsed_header["E_VERSION"] == 0x1:
+            print("E_VERSION         : 0x1 (VALID)")
+        else:
+            raise Exception("E_VERSION INVALID ({})".format(
+                parsed_header["E_VERSION"]))
+
+        print("Entry Point       : {}".format(hex(parsed_header["E_ENTRY"])))
+        print("Header table off. : {}".format(hex(parsed_header["E_PHOFF"])))
+        print("Section header off: {}".format(hex(parsed_header["E_SHOFF"])))
+        print("E_FLAGS           : {}".format(hex(parsed_header["E_FLAGS"])))
+        print("E_EHSIZE          : {}".format(hex(parsed_header["E_EHSIZE"])))
+        print("E_PHENTSIZE       : {}".format(hex(parsed_header["E_PHENTSIZE"])))
+        print("E_PHNUM           : {}".format(hex(parsed_header["E_PHNUM"])))
+        print("E_SHENTSIZE       : {}".format(hex(parsed_header["E_SHENTSIZE"])))
+        print("E_SHNUM           : {}".format(hex(parsed_header["E_SHNUM"])))
+        print("E_SHSTRNDX        : {}".format(hex(parsed_header["E_SHSTRNDX"])))
+
+        return parsed_header
 
 def build_elf_section(executable, offset, wordsize, size):
 
